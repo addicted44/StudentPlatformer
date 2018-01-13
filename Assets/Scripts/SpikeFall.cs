e469619c915c502515b4a6de68297d122b47f429
User-PC\Siyana <sianaromeova@gmail.com>
2018-01-13 16:00:22 +0200

    falling spike script

---
+++ Assets/Scripts/SpikeFall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeFall : MonoBehaviour {
+
+    public float fallSpeed = 8.0f;
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+    }
+}
